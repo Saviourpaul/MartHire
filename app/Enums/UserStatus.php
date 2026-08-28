8cba56e52aeca5c200d683a6fb1df6c1664df46f
saviour paul <saviourpaul24@gmail.com>
2026-08-28 15:15:01 +0100

    badgeClass

--- app/Enums/UserStatus.php
+++ app/Enums/UserStatus.php
@@ -22,4 +22,12 @@ enum UserStatus: string
             self::Suspended => 'Suspended',
         };
     }
+
+    public function badgeClass(): string
+    {
+        return match ($this) {
+            self::Active => 'bg-success-50 text-success-700 dark:bg-success-500/15 dark:text-success-400',
+            self::Suspended => 'bg-error-50 text-error-700 dark:bg-error-500/15 dark:text-error-400',
+        };
+    }
 }
