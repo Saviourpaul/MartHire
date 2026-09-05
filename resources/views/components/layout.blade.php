1c621472c29bd02bf1e96b50fb330eec93242366
saviour paul <saviourpaul24@gmail.com>
2026-09-05 23:54:35 +0100

    menu items icons (sidebar) wired

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -538,7 +538,6 @@ $currentUser = auth()->user();
                                 <!--Settings-->
                                 <li>
                                     <a href="{{ route('employer.settings') }}"
-                                        @click.prevent="selected = (selected === 'Settings' ? '':'Settings')"
                                         class="menu-item group"
                                         :class="(selected === 'Settings') ? 'menu-item-active' : 'menu-item-inactive'">
                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915"/><circle cx="12" cy="12" r="3"/></svg>
