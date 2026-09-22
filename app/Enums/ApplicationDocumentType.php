002f7a48a35c5427eab4757c15b2216c3fef6719
saviour paul <saviourpaul24@gmail.com>
2026-09-22 16:56:13 +0100

    updated user  identity type

--- app/Enums/ApplicationDocumentType.php
+++ app/Enums/ApplicationDocumentType.php
@@ -4,9 +4,30 @@ namespace App\Enums;
 
 enum ApplicationDocumentType: string
 {
-    case Nin = 'nin';
-    case Bvn = 'bvn';
+    case NationalIdentityCard = 'national_identity_card';
+    case InternationalPassport = 'international_passport';
+    case DriversLicense = 'drivers_license';
+    case VotersCard = 'voters_card';
     case Education = 'education';
+    case LegacyIdentity = 'legacy_identity';
+
+    /**
+     * @return list<self>
+     */
+    public static function identityTypes(): array
+    {
+        return [
+            self::NationalIdentityCard,
+            self::InternationalPassport,
+            self::DriversLicense,
+            self::VotersCard,
+        ];
+    }
+
+    public function isIdentityType(): bool
+    {
+        return in_array($this, self::identityTypes(), true);
+    }
 
     /**
      * @return list<string>
@@ -19,9 +40,12 @@ enum ApplicationDocumentType: string
     public function label(): string
     {
         return match ($this) {
-            self::Nin => 'National Identity Number',
-            self::Bvn => 'Bank Verification Number',
+            self::NationalIdentityCard => 'National Identity Card / NIN Slip',
+            self::InternationalPassport => 'International Passport',
+            self::DriversLicense => "Driver's License",
+            self::VotersCard => "Voter's Card",
             self::Education => 'Educational Qualification',
+            self::LegacyIdentity => 'Legacy identity document',
         };
     }
 }
